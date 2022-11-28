8c0017e7953b803dcb2d96ea4ddd9a10b22d0f27
kianamcc <42532725+kianamcc@users.noreply.github.com>
2022-11-27 21:06:05 -0800

    Journey Section Audio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
-import NavSection from './components/NavSection';
-import MainSection from './components/MainSection';
-import AboutSection from './components/AboutSection';
-import Product1 from './components/Product1Section';
-import Product2 from './components/Product2Section';
-import Science from './components/ScienceSection';
-import SignUpSection from './components/SignUpSection';
-import TestimonialsSection from './components/TestimonialsSection';
-import JourneySection from './components/JourneySection';
-import './components/TestimonialsSection.module.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import NavSection from "./components/NavSection";
+import MainSection from "./components/MainSection";
+import AboutSection from "./components/AboutSection";
+import Product1 from "./components/Product1Section";
+import Product2 from "./components/Product2Section";
+import Science from "./components/ScienceSection";
+import SignUpSection from "./components/SignUpSection";
+import TestimonialsSection from "./components/TestimonialsSection";
+import JourneySection from "./components/JourneySection";
+import "./components/TestimonialsSection.module.css";
 
-import classes from './App.module.css';
+import classes from "./App.module.css";
 
 function App() {
   return (
