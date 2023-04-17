23e60d39a915349fe1a867d44a0bda9604a59207
Vishrut Goel <v6goel@uwaterloo.ca>
2023-04-17 16:59:18 -0400

    changed text on about us page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -24,13 +24,17 @@ const About = () => {
 
           <Col>
             <div className="p-5 p-md-5 ">
-              <h3 className={appClasses.h3class}>Sentiment Analysis</h3>
+              <h3 className={appClasses.h3class}>About U4Ea</h3>
               <p className={appClasses.pclass}>
-                Founded in 2017 by Brandon Howard, U4Ea has helped more than
-                36,000 people across the world address physical, emotional,
-                and mental challenges by offering combinations of tones known
-                as <span className={classes.heavier}>binaural beats</span>,
-                enabling listeners to shift into healing states of mind.
+                Here at U4Ea, we believe that world peace begins with inner peace.
+                <br></br>
+                U4Ea is a collective of individuals coming together in the pursuit of sound as a healing modality. Be it mental, emotional, or physical pain or stress, history and science continue to support that bringing these states of being into balance promotes healing and sound is an accessible medium to do so.
+                <br></br>
+                Binaural Beats have been proven effective in 10 minutes or less and our 80% efficacy informs us that we are on the right path to taming the leading cause of cancer, stress. Enabling everyone to live a more balanced and peaceful life, full of ever-healthier choices.
+                <br></br>
+                In conjunction with The Verse and BioAcoustics Lab, U4Ea is developing clinically researched sound-based healing modalities in a variety of media formats; from mobile apps and video games to digital therapeutics and companion diagnostics.
+                <br></br>
+                Founded in 2017, U4Ea has helped more than 37,000 people across 96 countries address physical, emotional, and mental challenges by offering binaural beats, enabling listeners to shift into healing states of mind.
               </p>
             </div>
           </Col>
