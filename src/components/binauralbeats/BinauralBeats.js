e744cce8972cb417497ebf168c17b9a7b8090493
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-07-15 14:47:31 -0700

    UI almost done

--- src/components/binauralbeats/BinauralBeats.js
+++ src/components/binauralbeats/BinauralBeats.js
@@ -16,16 +16,21 @@ function BinauralBeats() {
   const [playing, setPlaying] = useState(false);
   const [freq1, setFreq1] = useState(0);
   const [freq2, setFreq2] = useState(0);
+
   const osc = useRef(null);
   const osc2 = useRef(null);
-  const onFeelChange = (e) => {
-    setFeel(e.target.value)
+
+  const onFeelChange = (newFeel) => {
+    console.log(`Feel changed to ${newFeel}`);
+    setFeel(newFeel);
   }
-  const onBoostChange = (e) => {
-    setBoost(e.target.value)
+  const onBoostChange = (newBoost) => {
+    console.log(`Boost changed to ${newBoost}`);
+    setBoost(newBoost);
   }
-  const onThingDuringChange = (e) => {
-    setThingDuring(e.target.value)
+  const onThingDuringChange = (newTD) => {
+    console.log(`TD changed to ${newTD}`);
+    setThingDuring(newTD)
   }
   const updateMap = (map, sf) => { // updates the values of the map with the given scale factor (used in the setFrequencies method)
     for(let [key, val] of map) {
@@ -73,34 +78,34 @@ function BinauralBeats() {
 
   const setFrequencies = () => {
     let param3MinusMap = new Map();
-    param3MinusMap.set("selfmotivate", 1.0);
-    param3MinusMap.set("improvemymood", 0.5);
-    param3MinusMap.set("meditate", 0.25);
-    param3MinusMap.set("relax", 0.125);
-    param3MinusMap.set("sleep", 0.0625);
+    param3MinusMap.set("Self Motivate", 1.0);
+    param3MinusMap.set("Improve my Mood", 0.5);
+    param3MinusMap.set("Meditate", 0.25);
+    param3MinusMap.set("Relax", 0.125);
+    param3MinusMap.set("Sleep", 0.0625);
     param3MinusMap.set("", 0.0); // this case will never happen when the user presses the button, however in the useEffect, this could happen, so this is error prevention
     switch (feel) {
-      case "oneness": setFreq1(60.19); break;
-      case "intuitive": setFreq1(53.25); break;
-      case "resolved": setFreq1(46.31); break;
-      case "harmonious": setFreq1(39.94); break;
-      case "miraculous": setFreq1(33.00); break;
-      case "cleansed": setFreq1(52.13); break;
-      case "liberation": setFreq1(49.5); break;
+      case "Oneness": setFreq1(60.19); break;
+      case "Intuitive": setFreq1(53.25); break;
+      case "Resolved": setFreq1(46.31); break;
+      case "Harmonious": setFreq1(39.94); break;
+      case "Miraculous": setFreq1(33.00); break;
+      case "Cleansed": setFreq1(52.13); break;
+      case "Liberation": setFreq1(49.5); break;
       default: setFreq1(0.0); break;
     }
     switch (boost) {
-      case "vitality": updateMap(param3MinusMap, 24.75); break;
-      case "creativity": updateMap(param3MinusMap, 26.06); break;
-      case "willpower": updateMap(param3MinusMap, 16.5); break;
-      case "love": updateMap(param3MinusMap, 19.97); break;
-      case "selfexpression": updateMap(param3MinusMap, 23.16); break;
-      case "focus": updateMap(param3MinusMap, 26.63); break;
-      case "consciousness": updateMap(param3MinusMap, 30.09); break;
-      case "painrelief": updateMap(param3MinusMap, 21.75); break;
-      case "cureall": updateMap(param3MinusMap, 20.00); break;
-      case "vigor": updateMap(param3MinusMap, 17.81); break;
-      case "rejuvenation": updateMap(param3MinusMap, 15.66); break;
+      case "Vitality": updateMap(param3MinusMap, 24.75); break;
+      case "Creativity": updateMap(param3MinusMap, 26.06); break;
+      case "Will Power": updateMap(param3MinusMap, 16.5); break;
+      case "Love": updateMap(param3MinusMap, 19.97); break;
+      case "Self-Expression": updateMap(param3MinusMap, 23.16); break;
+      case "Focus": updateMap(param3MinusMap, 26.63); break;
+      case "Consciousness": updateMap(param3MinusMap, 30.09); break;
+      case "Pain Relief": updateMap(param3MinusMap, 21.75); break;
+      case "Cure-All": updateMap(param3MinusMap, 20.00); break;
+      case "Vigor": updateMap(param3MinusMap, 17.81); break;
+      case "Rejuvenation": updateMap(param3MinusMap, 15.66); break;
       default: updateMap(param3MinusMap, 0.0); break;
     }
     setFreq2(freq1 - param3MinusMap[thingDuring]);
@@ -115,48 +120,54 @@ function BinauralBeats() {
                     <p className={classes.centertext}>I want to feel</p>
                   </Col>
                   <Col>
-                  <select value={feel} onChange={onFeelChange} className={classes.customdropdown}>
-                    <option value="">Select</option>
-                    <option value="oneness">Oneness</option>
-                    <option value="intuitive">Intuitive</option>
-                    <option value="resolved">Resolved</option>
-                    <option value="harmonious">Harmonious</option>
-                    <option value="miraculous">Miraculous</option>
-                    <option value="cleansed">Cleansed</option>
-                    <option value="liberation">Liberation</option>
-                  </select>
+                  <div className={classes.newdropdown}>
+                    <button className={classes.link}>{feel || 'Select'}</button>
+                    <div className={classes.newdropdownmenu} onClick={(e) => e.stopPropagation()} >
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Oneness")}}>Oneness</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Intuitive")}}>Intuitive</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Resolved")}}>Resolved</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Harmonious")}}>Harmonious</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Miraculous")}}>Miraculous</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Cleansed")}}>Cleansed</button>
+                      <button className={classes.dropdownoption} onClick={() => {onFeelChange("Liberation")}}>Liberation</button>
+                    </div>
+                  </div>
                   </Col>
                   <Col>
                     <p className={classes.centertext}>and boost</p>
                   </Col>
                   <Col>
-                    <select value={boost} onChange={onBoostChange} className={classes.customdropdown}>
-                      <option value="">Select</option>
-                      <option value="vitality">Vitality</option>
-                      <option value="creativity">Creativity</option>
-                      <option value="willpower">Will Power</option>
-                      <option value="love">Love</option>
-                      <option value="selfexpression">Self-Expression</option>
-                      <option value="focus">Focus</option>
-                      <option value="consciousness">Consciousness</option>
-                      <option value="painrelief">Pain Relief</option>
-                      <option value="cureall">Cure-All</option>
-                      <option value="vigor">Vigor</option>
-                      <option value="rejuvenation">Rejuvenation</option>
-                    </select>
+                    <div className={classes.newdropdown}>
+                      <button className={classes.link}>{boost || 'Select'}</button>
+                      <div className={classes.newdropdownmenu} onClick={(e) => e.stopPropagation()} >
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Vitality")}}>Vitality</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Creativity")}}>Creativity</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Will Power")}}>Will Power</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Love")}}>Love</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Self-Expression")}}>Self-Expression</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Focus")}}>Focus</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Consciousness")}}>Consciousness</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Pain Relief")}}>Pain Relief</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Cure-All")}}>Cure-All</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Vigor")}}>Vigor</button>
+                        <button className={classes.dropdownoption} onClick={() => {onBoostChange("Rejuvenation")}}>Rejuvenation</button>
+                      </div>
+                    </div>
                   </Col>
                   <Col>
                     <p className={classes.centertext}>while I</p>
                   </Col>
                   <Col>
-                    <select value={thingDuring} onChange={onThingDuringChange} className={classes.customdropdown}>
-                      <option value="">Select</option>
-                      <option value="selfmotivate">Self Motivate</option>
-                      <option value="improvemymood">Improve my Mood</option>
-                      <option value="meditate">Mediate</option>
-                      <option value="relax">Relax</option>
-                      <option value="sleep">Sleep</option>
-                    </select>
+                    <div className={classes.newdropdown}>
+                      <button className={classes.link}>{thingDuring || 'Select'}</button>
+                      <div className={classes.newdropdownmenu} onClick={(e) => e.stopPropagation()} >
+                        <button className={classes.dropdownoption} onClick={() => {onThingDuringChange("Self Motivate")}}>Self Motivate</button>
+                        <button className={classes.dropdownoption} onClick={() => {onThingDuringChange("Improve my Mood")}}>Improve my Mood</button>
+                        <button className={classes.dropdownoption} onClick={() => {onThingDuringChange("Mediate")}}>Mediate</button>
+                        <button className={classes.dropdownoption} onClick={() => {onThingDuringChange("Relax")}}>Relax</button>
+                        <button className={classes.dropdownoption} onClick={() => {onThingDuringChange("Sleep")}}>Sleep</button>
+                      </div>
+                    </div>
                   </Col>
                 </Row>
                 <Row>
@@ -175,3 +186,13 @@ function BinauralBeats() {
 
 
 export default BinauralBeats
+
+
+{/* <li onClick={() => onFeelChange('')}>Select</li>
+<li onClick={() => onFeelChange('oneness')}>Oneness</li>
+<li onClick={() => onFeelChange('intuitive')}>Intuitive</li>
+<li onClick={() => onFeelChange('resolved')}>Resolved</li>
+<li onClick={() => onFeelChange('harmonious')}>Harmonious</li>
+<li onClick={() => onFeelChange('miraculous')}>Miraculous</li>
+<li onClick={() => onFeelChange('cleansed')}>Cleansed</li>
+<li onClick={() => onFeelChange('liberation')}>Liberation</li> */}
