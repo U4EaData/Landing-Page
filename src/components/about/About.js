4e9bf4a33611b942993d4c0b319a01cfa9ec70e1
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-07-25 13:38:30 -0700

    About us done for the most part

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,40 +5,39 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { Fade } from "react-awesome-reveal";
+import appClasses from "../../App.module.css";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
   return (
-    <section className={classes.sectioncontainer}>
-      <Container className={classes.container}>
-        <Row>
-          <Col>
-            <img
-              src={greenCircle}
-              alt="green journey chakra"
-              className={classes.greencricleimage}
-            />
-          </Col>
-          <Col>
-            <h1 className={classes.bigtext}>The Healing Power of Sound</h1>
-            <div className={classes.textbox}>
-              <p className={classes.desctext}><b>Sound has been used to heal for thousands of years in cultures all around the world. Here at U4EA, we bring that power to you!</b></p>
-              <div className={classes.gap}/>
-              <p className={classes.desctext}>Binaural beats have been proven effective in 10 minutes or less and our 80% efficacy informs us that we are on the path to taming the leading cause of cancer, stress.</p>
-              <div className={classes.gap}/>
-              <p className={classes.desctext}>Enabling everyone to live a more balanced and peaceful life, full of ever-healthier choices.</p>
-            </div>
-            <Button
-              href="/binauralbeats"
-              variant="primary"
-              size="lg"
-              className={classes.buttonsize}
-            >
-              Experience
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <Fade duration={900} triggerOnce="true">
+      <section className={classes.sectioncontainer}>
+        <Container className={classes.container}>
+          <Row className={`${appClasses.mobileflex} ${classes.aboutFlexContainer} align-items-center`}>
+          <Col className="align-content-center">
+              <div className="d-flex justify-content-center ">
+                <img
+                  className={classes.image}
+                  src={greenCircle}
+                  alt="greenCircle"
+                ></img>
+              </div>
+            </Col>
+            <Col>
+              <h1 className={classes.bigtext}>About Us</h1>
+              <div className={classes.textbox}>
+                <p className={classes.desctext}><b>At U4EA, we belive that WORLD PEACE begins with INNER PEACE.</b></p>
+                <div className={classes.gap}/>
+                <p className={classes.desctext}>We use the science of music to make it healthier in supporting your journey to mental, emotional and physical balance.</p>
+                <div className={classes.gap}/>
+                <p className={classes.desctext}>We design our innovations to empower individuals with healing vibrations everywhere they go.</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Fade>
   );
 };
 
